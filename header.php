8e5bd9c9fcfe725d065d7c194ad31fdb80c0b86d
3steak <fc.barcelone94@hotmail.fr>
2022-02-09 22:06:38 +0100

    ajout du style.css  footer.php footer.css header.php et navbar.css

---
+++ header.php
@@ -0,0 +1,31 @@
+
+    <link rel="stylesheet" href="navbar.css"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AccessorySkate</title>
+
+<h1> AccessorySkate, le site d'accessoires et gadgets dont vous avez besoin !  </h1>
+
+<nav>
+<h2 class="logo"><a href="#">AccessorySkate</a></h2>
+
+<!-- liste ul pour connexion deco panier et fav ( si co ) -->
+     <ul class="menu">
+        <!-- inclure le logo du site -->
+
+
+        <!-- Connexion -->
+        <li class="connexion button"><a href="#">Connexion</a></li>
+
+        <!-- Deconnexion -->
+        <li class="deconnexion button secondary"><a href="#">Deconnexion</a></li> 
+
+        <!-- Panier --> 
+        <li class="panier"><a href="#"> <img alt="panier" src="image/navbar/panier.png"></a></li>
+
+        <!-- Favoris si connecté -->
+        <li class="fav"><a href="#">Favoris</a></li>
+
+    </ul>
+    
+</nav>
